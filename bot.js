6871124f85f83fd70ed90d6edcc30c4f59bcaf81
abdelnourgame <48439144+abdelnourgame@users.noreply.github.com>
2019-03-11 13:06:28 +0100

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,64 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By abdelnour");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("541653142161457152
+")
+setInterval(function() {
+channel.send(`
+love @!OS,   DaàààààààD :crown::crown::crown:#4367
+love @!OS,   DaàààààààD :crown::crown::crown:#4367
+love @!OS,   DaàààààààD :crown::crown::crown:#4367
+love @!OS,   DaàààààààD :crown::crown::crown:#4367
+love @!OS,   DaàààààààD :crown::crown::crown:#4367
+love @!OS,   DaàààààààD :crown::crown::crown:#4367
+love @!OS,   DaàààààààD :crown::crown::crown:#4367
+love @!OS,   DaàààààààD :crown::crown::crown:#4367
+love @!OS,   DaàààààààD :crown::crown::crown:#4367
+love @!OS,   DaàààààààD :crown::crown::crown:#4367
+love @!OS,   DaàààààààD :crown::crown::crown:#4367
+love @!OS,   DaàààààààD :crown::crown::crown:#4367
+love @!OS,   DaàààààààD :crown::crown::crown:#4367
+love @!OS,   DaàààààààD :crown::crown::crown:#4367
+love @!OS,   DaàààààààD :crown::crown::crown:#4367
+love @!OS,   DaàààààààD :crown::crown::crown:#4367
+love @!OS,   DaàààààààD :crown::crown::crown:#4367
+love @!OS,   DaàààààààD :crown::crown::crown:#4367
+love @!OS,   DaàààààààD :crown::crown::crown:#4367
+love @!OS,   DaàààààààD :crown::crown::crown:#4367
+love @!OS,   DaàààààààD :crown::crown::crown:#4367
+love @!OS,   DaàààààààD :crown::crown::crown:#4367
+love @!OS,   DaàààààààD :crown::crown::crown:#4367
+love @!OS,   DaàààààààD :crown::crown::crown:#4367
+love @!OS,   DaàààààààD :crown::crown::crown:#4367
+love @!OS,   DaàààààààD :crown::crown::crown:#4367
+love @!OS,   DaàààààààD :crown::crown::crown:#4367
+love @!OS,   DaàààààààD :crown::crown::crown:#4367
+love @!OS,   DaàààààààD :crown::crown::crown:#4367
+love @!OS,   DaàààààààD :crown::crown::crown:#4367
+love @!OS,   DaàààààààD :crown::crown::crown:#4367
+love @!OS,   DaàààààààD :crown::crown::crown:#4367
+love @!OS,   DaàààààààD :crown::crown::crown:#4367
+love @!OS,   DaàààààààD :crown::crown::crown:#4367
+love @!OS,   DaàààààààD :crown::crown::crown:#4367
+love @!OS,   DaàààààààD :crown::crown::crown:#4367
+love @!OS,   DaàààààààD :crown::crown::crown:#4367
+love @!OS,   DaàààààààD :crown::crown::crown:#4367
+love @!OS,   DaàààààààD :crown::crown::crown:#4367
+love @!OS,   DaàààààààD :crown::crown::crown:#4367
+love @!OS,   DaàààààààD :crown::crown::crown:#4367
+love @!OS,   DaàààààààD :crown::crown::crown:#4367
+love @!OS,   DaàààààààD :crown::crown::crown:#4367
+love @!OS,   DaàààààààD :crown::crown::crown:#4367
+love @!OS,   DaàààààààD :crown::crown::crown:#4367
+love @!OS,   DaàààààààD :crown::crown::crown:#4367
+love @!OS,   DaàààààààD :crown::crown::crown:#4367
+love @!OS,   DaàààààààD :crown::crown::crown:#4367
+love @!OS,   DaàààààààD :crown::crown::crown:#4367
+love @!OS,   DaàààààààD :crown::crown::crown:#4367`);
+}, 30)
+})
+
+client.login("NTUzODc5ODExMzc0OTcyOTQ4.D2ffYA.9fioiGvOSb7p_C-2JcmCX_ySD2c");
